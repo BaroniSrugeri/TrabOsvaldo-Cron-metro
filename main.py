ed3efa6da0ccdde57513abd137b7ca5055d48c5b
Baroni Rugeri <162318601+BaroniSrugeri@users.noreply.github.com>
2025-03-21 08:04:25 -0300

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import time
+import time 
 
 def cronometro(horas, minutos, segundos):
-    # Convertendo horas, minutos e segundos para total de segundos
     total_segundos = horas * 3600 + minutos * 60 + segundos
 
-    # Contagem regressiva
     for t in range(total_segundos, -1, -1):
         horas, resto = divmod(t, 3600)
         minutos, segundos = divmod(resto, 60)
         print(f"{horas:02d}:{minutos:02d}:{segundos:02d}", end="\r")
         time.sleep(1)
-    print("Tempo esgotado!")
+    print("Tempo esgotado")
 
-# Solicitar entrada do usuário
-print("Digite o tempo para o cronômetro:")
+print("Bem-vindo ao cronometro mais legal de todos!!")
+print("Digite um tempo para o seu cronometro:")
 horas = int(input("Horas: "))
 minutos = int(input("Minutos: "))
 segundos = int(input("Segundos: "))
 
-# Executar o cronômetro
 cronometro(horas, minutos, segundos)
